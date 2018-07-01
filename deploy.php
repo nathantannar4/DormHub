46686c99a4c36649f411591296d745ff37c66fa6
Nathan Tannar <nathantannar4@gmail.com>
2018-07-01 13:59:19 -0700

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -31,21 +31,3 @@
 		return $output;
 	}
 ?>
-<!-- <!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-	<meta charset="UTF-8">
-	<title>GIT DEPLOYMENT SCRIPT</title>
-</head>
-<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
-<pre>
- ____________________________
-|                            |
-| Git Deployment Script v0.1 |
-|      github.com/riodw 2017 |
-|____________________________|
-
-<?php echo $output; ?>
-</pre>
-</body>
-</html> -->
