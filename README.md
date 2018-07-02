e906286b3154089bd6ee8db3818a5b6f1baa3b3a
Nathan Tannar <nathantannar4@gmail.com>
2018-07-01 18:41:42 -0700

    Stability Fixes, updated SampleData.json

--- README.md
+++ README.md
@@ -6,7 +6,6 @@
 
 - MySQL
 - NodeJS 
-- PHP (for automated deployment)
 
 ## Start
 
@@ -14,8 +13,13 @@ Edit `app.js` to reflect your MySQL database connection parameters
 > You may need to create a new database. By default `app.js` connects to the `prod` database.
 
 ```
+npm install // To install the dependencies
 npm start
 ```
+
+## Automated Deployment
+
+Automated deployment is set up to automatically pull updates from a GitHub repo whenever it is pushed to. This is done by utilizing GitHubs Webhook functionality. Automated Deployment requires PHP and an ssh keypair to be generated on the server and added to your GitHub profile's keys
 
 ## Sample Data
 
--- app.js
+++ app.js
@@ -16,9 +16,9 @@ const pool = mysql.createPool({
     debug    :  false
 });
 
-function handleDatabaseConnectionError(response) {
-    response.json({"code": 100, "status": "Error in connection database"});
-}
+/***********************/
+/*** Helper Functions **/
+/***********************/
 
 function getConnection(callback) {
     pool.getConnection(function(error, connection) {
@@ -43,8 +43,17 @@ function queryDatabase(query, data, callback) {
     });
 }
 
+/***********************/
+/******* Express *******/
+/***********************/
+
+/// Create the Exporess web app
 const app = express();
 app.use(bodyParser.json())
+
+/***********************/
+/******* Routes ********/
+/***********************/
 
 app.get("/", function(request, response) {
     response.json({"code": 200, "status": "Welcome to DormHub"});
@@ -56,7 +65,10 @@ app.post("/deploy", function(request, response) {
         if (error) return response.json(error);
         // php now contain user defined php functions.
         php.deploy(function(error, result, output, printed){
-            if (error) return response.json(error);
+            if (error) {
+                console.log("> Deployment Failed");
+                return response.json(error);
+            }
             console.log("> Updates Deployed Successfully");
             response.json({"code": 200, "status": "DormHub Deployed"});
         });
@@ -67,47 +79,111 @@ app.post("/deploy", function(request, response) {
 app.get("/populate", function(request, response) {
 
     const sampleData = require("./SampleData.json");
-    for (const table of Object.keys(sampleData)) {
+    const tables = Object.keys(sampleData);
+    for (const table of tables) {
         // Drop current table
         queryDatabase(sampleData[table].SQL.drop, null, function(error, rows) {
+            if (error) { 
+                console.log(error);
+                return response.json({"code": 101, "status": "Failed to DROP table `" + table + "`"})
+            }
             // Create new table
             queryDatabase(sampleData[table].SQL.create, null, function(error, rows) {
+                if (error) {
+                    console.log(error);
+                    return response.json({"code": 101, "status": "Failed to CREATE table `" + table + "`"})
+                }
                 // Fill table with data
-                for (const j in sampleData[table].data) {
-                    queryDatabase(sampleData[table].SQL.insert, sampleData[table].data[j], function(error, rows, feilds) {
-                        if (error) console.log(error);
+                const isLastTable = table == tables[tables.length - 1];
+                const lastInsert = sampleData[table].data.length - 1;
+                for (const i in sampleData[table].data) {
+                    queryDatabase(sampleData[table].SQL.insert, sampleData[table].data[i], function(error, rows, fields) {
+                        if (error) { 
+                            console.log(error);
+                            return response.json({"code": 101, "status": "Failed to INSERT VALUES INTO table `" + table + "` with data"})
+                        } else {
+                            if (isLastTable && lastInsert == i) {
+                                response.json({"code": 200, "status": "SQL Database populated with `SampleData.json`"})
+                            }
+                        }
                     });
                 }
             });
         });
     }
-    response.json({"code": 200, "status": "Complete"})
 })
 
-/// Query for all User's 
-app.get("/users", function(request, response) {
-    queryDatabase("SELECT * FROM User", null, function(error, rows, fields) {
-        if (error) return handleDatabaseConnectionError(response);
+/// Query for all Seekers 
+app.get("/seekers", function(request, response) {
+    queryDatabase("SELECT * FROM Seeker", null, function(error, rows, fields) {
+        if (error) return response.json(error);
         response.json(rows);
     });
 })
 
-/// Query for User with specified ID
-app.get("/user/:userID", function(request, response) {
-    const userID = request.params.userID
-    queryDatabase("SELECT * FROM User WHERE id = " + userID, null, function(error, rows, fields) {
-        if (error) return handleDatabaseConnectionError(response);
+/// Query for Seeker with specified ID
+app.get("/seeker/:id", function(request, response) {
+    const id = request.params.id
+    queryDatabase("SELECT * FROM Seeker WHERE id = " + id, null, function(error, rows, fields) {
+        if (error) return response.json(error);
         response.json(rows);
     });
 })
 
-/// Query for all Reviews's 
+/// Query for all Landlords 
+app.get("/landlords", function(request, response) {
+    queryDatabase("SELECT * FROM Landlord", null, function(error, rows, fields) {
+        if (error) return response.json(error);
+        response.json(rows);
+    });
+})
+
+/// Query for Seeker with specified ID
+app.get("/landlord/:id", function(request, response) {
+    const id = request.params.id
+    queryDatabase("SELECT * FROM Landlord WHERE id = " + id, null, function(error, rows, fields) {
+        if (error) return response.json(error);
+        response.json(rows);
+    });
+})
+
+/// Query for all Reviews
 app.get("/reviews", function(request, response) {
     queryDatabase("SELECT * FROM Review", null, function(error, rows, fields) {
-        if (error) return handleDatabaseConnectionError(response);
+        if (error) return response.json(error);
         response.json(rows);
     });
 })
+
+/// Query for Review with specified ID
+app.get("/review/:id", function(request, response) {
+    const id = request.params.id
+    queryDatabase("SELECT * FROM Review WHERE id = " + id, null, function(error, rows, fields) {
+        if (error) return response.json(error);
+        response.json(rows);
+    });
+})
+
+/// Query for all Locations
+app.get("/locations", function(request, response) {
+    queryDatabase("SELECT * FROM Location", null, function(error, rows, fields) {
+        if (error) return response.json(error);
+        response.json(rows);
+    });
+})
+
+/// Query for all Location in specified city
+app.get("/location/:city", function(request, response) {
+    const city = request.params.city;
+    queryDatabase("SELECT * FROM Location WHERE city = " + city, null, function(error, rows, fields) {
+        if (error) return response.json(error);
+        response.json(rows);
+    });
+})
+
+/***********************/
+/****** Networking *****/
+/***********************/
 
 /// Start the app listening on port 3000
 app.listen(3000, function() {
