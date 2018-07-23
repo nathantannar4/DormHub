a812f5912a677e0a255bee0ce5cb5740d443dbf8
Nathan Tannar <nathantannar4@gmail.com>
2018-07-22 19:09:13 -0700

    Some basic CSS styling

--- app/src/components/NavigationBar.js
+++ app/src/components/NavigationBar.js
@@ -1,25 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import './NavigationBar.css';
+import NavLink from './NavLink'
 
-class NavigationBar extends React.Component {
+class NavigationBar extends Component {
 
   render() {
-    return (
-      <header>
-        <nav>
-          <ul>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/persons'>Persons</Link></li>
-            <li><Link to='/seekers'>Seekers</Link></li>
-            <li><Link to='/landlords'>Landlords</Link></li>
-            <li><Link to='/housing'>Housing</Link></li>
-            <li><Link to='/homeSwappers'>Home Swappers</Link></li>
-            <li><Link to='/reviews'>Reviews</Link></li>
-            <li><Link to='/ratings'>Ratings</Link></li>
-            <li><Link to='/locations'>Location</Link></li>
-          </ul>
-        </nav>
-      </header>
+    return (  
+      <div className="header">
+        <h1>DormHub</h1>
+        <div className="header-right">
+          <Link to='/'>Home</Link>
+          <NavLink to='/persons'>Persons</NavLink>
+          <NavLink to='/seekers'>Seekers</NavLink>
+          <NavLink to='/landlords'>Landlords</NavLink>
+          <NavLink to='/housing'>Housing</NavLink>
+          <NavLink to='/homeSwappers'>Home Swappers</NavLink>
+          <NavLink to='/reviews'>Reviews</NavLink>
+          <NavLink to='/ratings'>Ratings</NavLink>
+          <NavLink to='/locations'>Location</NavLink>
+        </div>
+      </div>
     )
   }
 }
--- app/src/components/Table.js
+++ app/src/components/Table.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Table.css'
 
 class Table extends Component {
 
